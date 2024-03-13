9b4cd9f8998f81b07210639043b4282b47a68774
RubensJr21 <junior.correa2001@gmail.com>
2024-03-13 13:29:55 -0300

    Adiciona inoformações solicitadas pelo professor

--- 1.robo_aspirador/main.py
+++ 1.robo_aspirador/main.py
@@ -1,3 +1,8 @@
+# Nome do Aluno/a: Rubens do Nascimento Corrêa Júnior
+# Disciplina/semestre: Inteligência Artificial/2024-1
+# Turma: 37971
+# Professor: Sergio Nery Simões
+
 from classes.local import Local
 from classes.ambiente import AmbienteA, AmbienteB
 from classes.robo import Robo
